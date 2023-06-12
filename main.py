e3e1ee8d86a565a3ab37f1acf7f796184003913d
Veter44k <Veter44k@yandex.ru>
2023-06-12 15:23:38 +0300

    hello_world()

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Привет, мастер джедай!")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Привет, мой первый проект!'
+
+
+if __name__ == '__main__':
+    app.run()
